1db0de995eb60a8a3c9a3a16780570e03a7259e8
Avraam Tsantekidis <makistsantekidis@gmail.com>
2015-04-15 16:53:50 +0300

    Trying to make IRNN work

--- piano_experiment.py
+++ piano_experiment.py
@@ -6,13 +6,14 @@ from theano import config
 floatX = config.floatX
 import numpy as np
 from theano import tensor as T
-from blocks.bricks.recurrent import SimpleRecurrent, recurrent, BaseRecurrent
+from blocks.bricks.recurrent import SimpleRecurrent, recurrent, BaseRecurrent, LSTM
 
-from utils import MismulitclassificationRate, NegativeLogLikelihood
+from utils import MismulitclassificationRate, MeanSquare, ParametricRectifier, MismulitmistakeRate, NanRectify
 from blocks.bricks.cost import CategoricalCrossEntropy, SquaredError
-from blocks.initialization import IsotropicGaussian, Constant
+from blocks.initialization import IsotropicGaussian, Constant, Identity
 from blocks.algorithms import GradientDescent, Adam, Scale, CompositeRule, StepClipping, RemoveNotFinite
-from blocks.bricks import Tanh, MLP, Linear, Sigmoid, Rectifier
+from blocks.bricks import Tanh, MLP, Linear, Sigmoid, Rectifier, WEIGHT, BIAS
+from blocks import bricks
 from blocks.main_loop import MainLoop
 from fuel.streams import DataStream
 from fuel.schemes import SequentialScheme
@@ -23,14 +24,22 @@ from blocks.extensions import Timing, Printing, FinishAfter
 from blocks.extensions.monitoring import TrainingDataMonitoring, DataStreamMonitoring
 from blocks.monitoring import aggregation
 from blocks.extensions.plot import Plot
+from blocks.extensions.saveload import Checkpoint
+
+from blocks.filter import VariableFilter
 
 from blocks.graph import ComputationGraph
+from blocks.config_parser import config
+
+config.recursion_limit = 100000
 
 
 def test_value(variable, test_val):
     variable.tag.test_value = test_val
     return variable
 
+
+np.set_printoptions(threshold='nan')
 
 x = T.tensor3('features')
 x_mask = T.matrix('features_mask')
@@ -49,37 +58,53 @@ class SimpleRNN(BaseRecurrent):
         super(SimpleRNN, self).__init__(**kwargs)
         self.dim = dims[0]
         self.dims = dims
-        # self.in_layer = MLP(activations=[Linear(dims[0], dims[1])], dims=[dims[0], dims[1]],
-        # weights_init=IsotropicGaussian(0.01),
-        # biases_init=Constant(0.0),
-        # name="in_layer")
-        self.in_layer = Linear(dims[0], dims[1],
-                               weights_init=IsotropicGaussian(0.01),
-                               biases_init=Constant(0.0),
+
+        self.in_layer = Linear(input_dim=dims[0], output_dim=dims[1],
+                               weights_init=IsotropicGaussian(0.001),
+                               use_bias=False,
+                               # biases_init=Constant(0.0),
                                name="in_layer")
 
-        self.rnn_layer = SimpleRecurrent(dim=dims[1], activation=Tanh(),
-                                         weights_init=IsotropicGaussian(0.01),
+        self.rnn_layer = SimpleRecurrent(dim=dims[1], activation=NanRectify(),
+                                         weights_init=Identity(0.01),
                                          biases_init=Constant(0.0),
+                                         use_bias=True,
                                          name="rnn_layer")
 
+        # self.in_layer = Linear(input_dim=dims[0], output_dim=dims[1] * 4,
+        # weights_init=IsotropicGaussian(0.1),
+        # biases_init=Constant(0.0),
+        # name="in_layer")
+        #
+        # self.rnn_layer = LSTM(dim=dims[1], activation=Tanh(),
+        # weights_init=IsotropicGaussian(0.02),
+        # biases_init=Constant(0.0),
+        # use_bias=True,
+        # name="rnn_layer")
+
         self.out_layer = MLP(activations=[Sigmoid()], dims=[dims[1], dims[2]],
-                             weights_init=IsotropicGaussian(0.01),
-                             biases_init=Constant(0.0),
+                             weights_init=IsotropicGaussian(0.001),
+                             use_bias=False,
+                             # biases_init=Constant(0.0),
                              name="out_layer")
 
         self.children = [self.in_layer, self.rnn_layer, self.out_layer]
 
-    @recurrent(sequences=['inputs'], contexts=[], states=['hidden_state'],
+    @recurrent(sequences=['inputs', 'input_mask'], contexts=[], states=['hidden_state'],
                outputs=['hidden_state', 'output'])
-    def apply(self, inputs, hidden_state=None, output=None):
+    def apply(self, inputs, input_mask, hidden_state=None, cells=None, output=None):
         h_in = self.in_layer.apply(inputs)
-        hidden_state = self.rnn_layer.apply(inputs=h_in, states=hidden_state, iterate=False)
+        # hidden_state, cells = self.rnn_layer.apply(inputs=h_in, states=hidden_state, cells=cells, mask=input_mask,
+        # iterate=False)
+        hidden_state = self.rnn_layer.apply(inputs=h_in, states=hidden_state, mask=input_mask, iterate=False)
         output = self.out_layer.apply(hidden_state)
+        # return hidden_state, cells, output
         return hidden_state, output
 
     def get_dim(self, name):
+        # dims = dict(zip(['inputs', 'hidden_state', 'output'], self.dims))
         dims = dict(zip(['inputs', 'hidden_state', 'output'], self.dims))
+        dims['cells'] = dims['hidden_state']
         return dims.get(name, None) or super(SimpleRNN, self).get_dim(name)
 
 
@@ -91,7 +116,7 @@ def get_datastream(dataset):
     dataset = DataStream(
         dataset,
         iteration_scheme=SequentialScheme(
-            dataset.num_examples, 300
+            dataset.num_examples, 512
         ),
     )
     dataset = Padding(dataset)
@@ -106,41 +131,67 @@ def get_datastream(dataset):
 train = get_datastream(train)
 test = get_datastream(test)
 
-srnn = SimpleRNN(dims=[93, 200, 93])
+srnn = SimpleRNN(dims=[93, 128, 93])
 srnn.initialize()
 
-probs = srnn.apply(inputs=x)[1]
-probs = (probs.dimshuffle(2, 0, 1) * x_mask).dimshuffle(1, 2, 0)
-target = (y.dimshuffle(2, 0, 1) * y_mask).dimshuffle(1, 2, 0)
-cost = NegativeLogLikelihood().apply(target, probs, y_mask)
+hidden_states, probs = srnn.apply(inputs=x, input_mask=x_mask)
+
+debug_val = T.sum(T.isnan(hidden_states))
+debug_val.name = 'hidden_states_NANS'
+
+# hidden_states = T.sum(T.isnan(hidden_states))
+hidden_states = hidden_states[-1].mean()
+hidden_states.name = 'hidden_states'
+
+target = y  # - T.neq(y, 1)
+# probs = (probs.dimshuffle(2, 0, 1) * x_mask).dimshuffle(1, 2, 0)
+# target = (y.dimshuffle(2, 0, 1) * y_mask).dimshuffle(1, 2, 0)
+cost = MeanSquare().apply(target, probs, y_mask)
+# cost = SquaredError().apply(target, probs)
 error_rate = MismulitclassificationRate().apply(target, probs, y_mask)
+error_rate.name = "error on note as a whole"
+mistake_rate = MismulitmistakeRate().apply(target, probs, y_mask)
+mistake_rate.name = "single error within note"
 model = Model(cost)
 cg = ComputationGraph([cost])
-cost.name = "final_cost"
-step_rule = CompositeRule([RemoveNotFinite(), Adam(), StepClipping(6.0)])  # Scale(0.01)
 
+## l2/l1 regularization
+reg = 0.1
+params = VariableFilter(roles=[WEIGHT, BIAS])(cg.variables)
+for p in params:
+    cost += reg * abs(p).sum()
+    # cost += reg * (p ** 2).sum()
+cost.name = "final_cost"
+
+
+## Training algorithm
+step_rule = CompositeRule([RemoveNotFinite(), StepClipping(20.0), Adam(learning_rate=0.001), StepClipping(6.0),
+                           RemoveNotFinite()])  # Scale(0.01)
 algorithm = GradientDescent(step_rule=step_rule, cost=cost, params=cg.parameters)
 
-extensions = [FinishAfter(after_n_epochs=1000),
+extensions = [FinishAfter(after_n_epochs=5000),
               DataStreamMonitoring(
-                  [cost, error_rate],
-                  test,
+                  [cost, error_rate, mistake_rate],
+                  data_stream=test,
                   prefix="test"),
               TrainingDataMonitoring(
-                  [cost,
+                  [cost, hidden_states, debug_val,
                    aggregation.mean(algorithm.total_gradient_norm)],
                   prefix="train",
                   after_epoch=True),
               Timing(),
-              Printing()]
+              Printing(),
+              Checkpoint('piano_experiment_mainloop')]
 bokeh = True
 if bokeh:
     extensions.append(Plot(
         'Testing blocks',
-        channels=[['test_mismulitclassificationrate_apply_error_rate'],
-                  ['train_total_gradient_norm'],
-                  ['test_final_cost', 'train_final_cost'],
-                  ]))
+        channels=[
+            ['train_hidden_states_NANS'],
+            ['test_error on note as a whole', 'test_single error within note'],
+            ['test_final_cost', 'train_final_cost'],
+            ['train_total_gradient_norm'],
+        ]))
 
 main_loop = MainLoop(algorithm=algorithm,
                      data_stream=train,
--- utils.py
+++ utils.py
@@ -1,28 +1,65 @@
 __author__ = 'mike'
 
 import theano
-from theano import ifelse
+from theano.ifelse import ifelse
 from theano import tensor as T
 from blocks.bricks.cost import Cost
 from blocks.bricks.base import application, Brick
+from blocks.bricks import Activation
+from blocks.roles import add_role, WEIGHT, BIAS
 
 floatX = theano.config.floatX
 
+threshold = 0.5
 
 class MismulitclassificationRate(Cost):
     @application(outputs=["error_rate"])
     def apply(self, y, y_hat, y_mask=None):
         if y_mask:
-            result = (T.sum(T.max(T.neq(y, T.ge(y_hat, 0.5)), axis=2) * y_mask) /
+            result = (T.sum(T.max(T.neq(y, T.ge(y_hat, threshold)), axis=2) * y_mask) /
                       (T.sum(y_mask).astype(floatX)))
         else:
-            result = (T.sum(T.max(T.neq(y, T.ge(y_hat, 0.5)), axis=2)) /
+            result = (T.sum(T.max(T.neq(y, T.ge(y_hat, threshold)), axis=2)) /
+                      (y.shape[0] * y.shape[1]).astype(floatX))
+
+        return result
+
+class MismulitmistakeRate(Cost):
+    @application(outputs=["error_rate"])
+    def apply(self, y, y_hat, y_mask=None):
+        if y_mask:
+            result = (T.sum(T.sum(T.neq(y, T.ge(y_hat, threshold)), axis=2) * y_mask) /
+                      ((T.sum(y_mask)*y.shape[2]).astype(floatX)))
+        else:
+            result = (T.sum(T.max(T.neq(y, T.ge(y_hat, threshold)), axis=2)) /
                       (y.shape[0] * y.shape[1]).astype(floatX))
 
         return result
 
 
-class NegativeLogLikelihood(Cost):
+class MeanSquare(Cost):
     @application(outputs=["cost"])
     def apply(self, y, y_hat, y_mask):
-        return T.mean(-T.sum(y * T.log(abs(y_hat)+1e-14), axis=2)*y_mask)
+        return T.mean(T.sum((y - y_hat) ** 2, axis=2) * y_mask)
+
+class MeanSquare(Cost):
+    @application(outputs=["cost"])
+    def apply(self, y, y_hat, y_mask):
+        return T.mean(T.sum((y - y_hat) ** 2, axis=2) * y_mask)
+
+class NanRectify(Activation):
+    @application(inputs=['input_'], outputs=['output'])
+    def apply(self, input_):
+        input_ = T.switch(T.isnan(input_), 0, input_)
+        return T.switch(input_ > 0, input_, 0.01*input_)
+
+class ParametricRectifier(Activation):
+    def __init__(self, leaky_init, *args, **kwargs):
+        super(ParametricRectifier, self).__init__(*args, **kwargs)
+        self.leaky_init = leaky_init
+        add_role(self.leaky_init, WEIGHT)
+
+    @application(inputs=['input_'], outputs=['output'])
+    def apply(self, input_):
+        res = T.switch(input_ > 0, input_, self.leaky_init * input_)
+        return T.switch(T.isnan(res), 0, res)
